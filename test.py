f44081d56fb6d0f58dcffdaf962735d13dbfa2a2
killsystem10 <killsystem10@outlook.com>
2023-01-06 16:25:38 +0900

    Modify test.py

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
 import time
 import random
-list=[0,0,1,2,3,4,5,6,7,8,9]
-print("너의 숫자 입력 실력을 알아본다!")
-print("랜덤 16개의 숫자를 10번 입력하라")
-temp=input("엔터 시 시작")
-for i in range(1,11):
-    print(i, "번째 문제")
-    for temp in range(16):
-        word=random.randint(0,9)
-        print(word)
+right=0
+fall=0
+print("====숫자 빨리치기====")
+temp=input("엔터시 시작")
+firsttime=time.time()
+for i in range(10):
+    word=random.randint(0,9)
+    print(word
+          )
+    while True:
+        answer=input("숫자 입력=")
+        answer=int(answer)
+        if answer==word:
+            print("정답")
+            right=right+1
+            break
+        else:
+            print("틀림, 다시시도")
+            fall=fall+1
+            continue
+secondtime=time.time()
+finaltime=secondtime-firsttime
+print("맞은 횟수="+str(right), "틀린 횟수="+str(fall), "걸린시간="+str(finaltime))
+
+
